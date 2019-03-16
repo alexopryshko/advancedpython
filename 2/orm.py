268f191af6b5687d0299b1f196c60f48f534406e
Alex Opryshko <alexopryshko@yandex.ru>
2019-03-16 15:50:29 +0300

    lesson 2 orm

---
+++ 2/orm.py
@@ -0,0 +1,91 @@
+class Field:
+    def __init__(self, f_type, required=True, default=None):
+        self.f_type = f_type
+        self.required = required
+        self.default = default
+
+    def validate(self, value):
+        if value is None and not self.required:
+            return None
+        # todo exceptions
+        return self.f_type(value)
+
+
+class IntField(Field):
+    def __init__(self, required=True, default=None):
+        super().__init__(int, required, default)
+
+
+class StringField(Field):
+    def __init__(self, required=True, default=None):
+        super().__init__(str, required, default)
+
+
+class ModelMeta(type):
+    def __new__(mcs, name, bases, namespace):
+        if name == 'Model':
+            return super().__new__(mcs, name, bases, namespace)
+
+        meta = namespace.get('Meta')
+        if meta is None:
+            raise ValueError('meta is none')
+        if not hasattr(meta, 'table_name'):
+            raise ValueError('table_name is empty')
+
+        # todo mro
+
+        fields = {k: v for k, v in namespace.items()
+                  if isinstance(v, Field)}
+        namespace['_fields'] = fields
+        namespace['_table_name'] = meta.table_name
+        return super().__new__(mcs, name, bases, namespace)
+
+
+class Manage:
+    def __init__(self):
+        self.model_cls = None
+
+    def __get__(self, instance, owner):
+        if self.model_cls is None:
+            self.model_cls = owner
+        return self
+
+    def create(self):
+        print(self.model_cls)
+
+
+class Model(metaclass=ModelMeta):
+    class Meta:
+        table_name = ''
+
+    objects = Manage()
+    # todo DoesNotExist
+
+    def __init__(self, *_, **kwargs):
+        for field_name, field in self._fields.items():
+            value = field.validate(kwargs.get(field_name))
+            setattr(self, field_name, value)
+
+
+class User(Model):
+    id = IntField()
+    name = StringField()
+
+    class Meta:
+        table_name = ''
+
+
+class Man(User):
+    sex = StringField()
+
+
+user = User(id=1, name='name')
+User.objects.create(id=1, name='name')
+User.objects.update(id=1)
+User.objects.delete(id=1)
+
+User.objects.filter(id=2).filter(name='petya')
+
+user.name = '2'
+user.save()
+user.delete()
